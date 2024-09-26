90ecbd071a6c00e43eb49fc12016a8e858fb02a2
Rithiellybecker <124898795+Rithiellybecker@users.noreply.github.com>
2024-09-25 23:47:48 -0300

    melhorias no código
    melhorias no código para atender os critérios do desafio

--- main.py
+++ main.py
@@ -2,6 +2,9 @@ def sistema():
     
     saldo = 0
     extrato = []
+    limite_saque = 500  # Limite máximo por saque
+    numero_saques = 0  # Número de saques realizados no dia
+    max_saques = 3 
     
     print("Escolha uma opção: ")
     
@@ -13,18 +16,27 @@ def sistema():
         
         if opcao == 1:
             valor = float(input("Digite o valor a ser depositado: "))
-            saldo += valor
-            extrato.append(f"Deposito de: R$ {valor:.2f}") #.append modifica o valor do extrato
-            print(f"Valor depositado: R$ {valor:.2f}") #.2f representa duas casas decimais depois da virgula
-                    
-        elif opcao == 2:
-            saque = float(input("Digite o valor a ser sacado: "))
-            if saque > saldo: #faz a verificação se a operação e verdadeira
-                print("Saldo insuficiente!")
+            if valor <= 0:  # Verifica se o valor é negativo ou zero
+                print("Depósito inválido! O valor deve ser maior que R$ 0,00.")
             else:
-                saldo -= saque
-                extrato.append(f"Saque: R$ {saque:.2f}")
-                print(f"Valor sacado: R$ {saque:.2f}")
+                saldo += valor
+                extrato.append(f"Depósito de: R$ {valor:.2f}")#.append modifica o valor do extrato
+                print(f"Valor depositado: R$ {valor:.2f}")#.2f representa duas casas decimais depois da virgula
+        
+        elif opcao == 2:
+            if numero_saques >= max_saques:
+                print("Número máximo de saques atingido para hoje.")
+            else:
+                saque = float(input("Digite o valor a ser sacado: "))
+                if saque > saldo:
+                    print("Saldo insuficiente!")
+                elif saque > limite_saque:
+                    print(f"Valor máximo para saque é R$ {limite_saque:.2f}.")
+                else:
+                    saldo -= saque
+                    extrato.append(f"Saque: R$ {saque:.2f}")
+                    numero_saques += 1 
+                    print(f"Valor sacado: R$ {saque:.2f}")
             
         elif opcao == 3:
             print("\n --- Extrato ---")
